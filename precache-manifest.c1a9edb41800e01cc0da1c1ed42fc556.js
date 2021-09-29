0340ea06df777a38ca03416e51d82d46d539322d
Jesus Vazquez <jesusvazquez@jesuss-mac-mini.lan>
2021-09-29 14:48:56 -0600

    Updates

---
+++ precache-manifest.c1a9edb41800e01cc0da1c1ed42fc556.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60acf56554ed1d8719aada95aab49d8b",
+    "url": "/Inspection/index.html"
+  },
+  {
+    "revision": "eb102130f06ada9e172f",
+    "url": "/Inspection/static/css/2.12da0d42.chunk.css"
+  },
+  {
+    "revision": "65cf4857395efc146321",
+    "url": "/Inspection/static/css/main.f174cb6a.chunk.css"
+  },
+  {
+    "revision": "eb102130f06ada9e172f",
+    "url": "/Inspection/static/js/2.82946d03.chunk.js"
+  },
+  {
+    "revision": "65cf4857395efc146321",
+    "url": "/Inspection/static/js/main.956396f0.chunk.js"
+  },
+  {
+    "revision": "59ac575aa3b20a5c7ccf",
+    "url": "/Inspection/static/js/runtime~main.ead190f7.js"
+  },
+  {
+    "revision": "c0c0c1891f3c86bb1473450287cf5000",
+    "url": "/Inspection/static/media/QAInspector.c0c0c189.svg"
+  },
+  {
+    "revision": "dda3680b4c90e43a8ae0614cb8304148",
+    "url": "/Inspection/static/media/houses.dda3680b.svg"
+  },
+  {
+    "revision": "643ebbbd2b6715dc876f80788d7b574d",
+    "url": "/Inspection/static/media/ndt.643ebbbd.png"
+  }
+]);
